e5e15f96d117b3cfacf8ee98cfee317d83bb7855
Blackchick15 <109077737+Blackchick15@users.noreply.github.com>
2023-01-28 18:00:58 +0200

    commit

---
+++ practice.js
@@ -0,0 +1,62 @@
+// Задача №1.1
+// Створити три змінні
+// bananas = 100;
+// oranges = 30;
+// Сума у змінній sum
+// і вивести результат в консоль
+
+// const bananas = 100;
+// const oranges = 30;
+// const result = bananas + oranges
+
+// console.log(result)
+// Задача №1.2
+// Створити три змінні
+// strawberry = 20;
+// cherries = 15;
+// Різниця у змінній difference
+// і вивести результат в консоль
+// const strawberry = 20;
+// const cherries = 15;
+// const result = strawberry * cherries;
+
+// console.log(result)
+
+// ..............
+
+// Задача №2
+// Обєкт Math
+// Визнгачити площу круга
+// з радіусом 10см(ПR(2)).Math.pow(a, b) - піднести до степені
+//  const rad = 10;
+//  const PI = Math.PI;
+//  const powRad = Math.pow( rad, 2);
+//  const result = PI * powRad;
+
+//  console.log(result);
+
+
+
+// Задача №2.1
+// Обєкт Math
+// Визнгачити площу круга
+// з радіусом 20см(ПR(3)).Math.pow(a, b) - піднести до степені
+
+// const rad = 20;
+// const PI = Math.PI
+// const powRad = Math.pow( rad, 3);
+// const result = PI * powRad;
+
+// console.log(result);
+
+
+// Задача №2.2
+// Обєкт Math
+// Визнгачити площу круга
+// з радіусом 100см(ПR(2)).Math.pow(a, b) - піднести до степені
+// const rad = 100;
+// const PI = Math.PI
+// const powRad = Math.pow( rad, 2);
+// const result = PI * powRad;
+
+// console.log(result);
